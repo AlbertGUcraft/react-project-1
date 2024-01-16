fea6138fa135d933680259ddbbc9c98644210fb6
Albert <albert.gasparyandxm@gmail.com>
2024-01-16 18:06:16 +0400

    user page implement

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,8 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import '../index.css';
 function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('https://reqres.in/api/users');
+        const data = await response.json();
+        setUsers(data.data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
-    <div>
-      <p>here will be user data</p>
+    <div className="user-list">
+      {users.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
     </div>
   );
 }
+
+const UserCard = ({ user }) => {
+  const { first_name, last_name, email, avatar } = user;
+
+  return (
+    <div className="user-card">
+      <div>
+        <img src={avatar} alt={`${first_name} ${last_name}`} />
+        <h3>{`${first_name} ${last_name}`}</h3>
+        <p>{email}</p>
+      </div>
+    </div>
+  );
+};
+
 export default Users;
